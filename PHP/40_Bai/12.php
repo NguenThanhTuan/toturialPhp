11afb8b01e263cde404842d306d6595887cc7f8a
ttxuyenkimgiap <thanhtuannc95@gmail.com>
2017-12-25 00:44:44 +0700

    fix

--- PHP/40_Bai/12.php
+++ PHP/40_Bai/12.php
@@ -1,17 +1,18 @@
 <?php 
-    $num = array(1000, 1200, 1400);
+    $num = array("1000", "1200", "1400");
 
  ?>
  <table border="1" cellpadding="0" cellspacing="0">
     <tr>
-        <th><font color="blue">Salary of Mr. A is</font></td>
-        <td><font color="blue">Salary of Mr. B is</font></td>
-        <td><font color="blue">Salary of Mr. C is</font></td>
+        <td><font color="blue">Salary of Mr. A is</font></td>
+        <td><?php echo $num[0]."$"; ?></td>       
     </tr>
     <tr>
-        <th>Salary of Mr. A is</td>
-        <td>Salary of Mr. A is</td>
-        <td>Salary of Mr. A is</td>
+        <td><font color="blue">Salary of Mr. B is</font></td>
+        <td><?php echo $num[1]."$"; ?></td>
     </tr>
-     
+    <tr>
+        <td><font color="blue">Salary of Mr. C is</font></td>        
+        <td><?php echo $num[2]."$"; ?></td>
+    </tr>
  </table>
